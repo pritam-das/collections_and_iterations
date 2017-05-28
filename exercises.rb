c94d289e700939e3dbbbf63cd82874aa1eed8648
pritam-das <pritam.das@mail.utoronto.ca>
2017-05-28 18:24:43 -0400

    Exercise 0 complete

---
+++ exercises.rb
@@ -0,0 +1,10 @@
+#Exercise 0
+
+#1
+fav_colours = ["blue", "black", "yellow"]
+my_age= [25, 28, 21]
+coin_flip_results= ["H", "H", "H", "T", "H"]
+fav_artists = ["pink floyd", "muse", "stereophonics"]
+
+#2
+hash1 = {:onomatopoeia => "the formation of a word from a sound associated with what is named", :sympathy => "feelings of pity and sorrow for someone else's misfortune", :empathy => "the ability to understand and share the feelings of another"}
